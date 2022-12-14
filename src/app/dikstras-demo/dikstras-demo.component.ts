662259a6254defec83e0460081c3f6f0285b6e4e
Clayton Kristiansen <clayton.kristiansen@hotmail.com>
2022-12-13 20:25:22 -0600

    Configured heap sort script to work

--- src/app/dikstras-demo/dikstras-demo.component.ts
+++ src/app/dikstras-demo/dikstras-demo.component.ts
@@ -7,19 +7,11 @@ import { Router } from '@angular/router';
   styleUrls: ['./dikstras-demo.component.css']
 })
 export class DikstrasDemoComponent {
-  gridPainterScript: HTMLScriptElement;
-  dikstrasScript: HTMLScriptElement;
   mainScript: HTMLScriptElement;
 
   constructor(private router: Router) {
-    // this.gridPainterScript = document.createElement("script");
-    // this.gridPainterScript.src = "../../assets/scripts/GridPainter.js";
-    // this.dikstrasScript = document.createElement("script");
-    // this.dikstrasScript.src = "../../assets/scripts/Dikstras.js";
     this.mainScript = document.createElement("script");
     this.mainScript.src = "../../assets/scripts/main.js";
-    // document.body.appendChild(this.gridPainterScript);
-    // document.body.appendChild(this.dikstrasScript);
     document.body.appendChild(this.mainScript);
   }
 
